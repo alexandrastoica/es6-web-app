26c08be99dcc46acf32a8251c2dbf25977a623de
Alexandra Stoica <alexandrasto95@gmail.com>
2016-11-14 15:24:17 +0000

    add routing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import { AppBase } from './classes/app-base.js';
+import { HomePage } from './pages/home-page.js';
+import { ProfilePage } from './pages/profile-page.js';
+import { ListsPage } from './pages/lists-page.js';
 import $ from 'jquery';
 
 export class App extends AppBase {
   constructor(){
     super('Title'); //pass title for appbase
+
+    //add routing
+    this.addRoute('Home', new HomePage(), true); //true for default page
+    this.addRoute('Profile', new ProfilePage());
+    this.addRoute('Lists', new ListsPage());
   }
 }
 
